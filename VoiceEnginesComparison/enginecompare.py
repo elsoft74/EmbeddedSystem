b17ccb44466a12de8fca8135c6092f88f70eeb14
Ivo Pugliese <elsoft74@gmail.com>
2021-01-30 17:07:15 +0100

    Cambio del critrerio di registrazione, ora si usa la classe VADAudio da DeepSpeech

--- VoiceEnginesComparison/enginecompare.py
+++ VoiceEnginesComparison/enginecompare.py
@@ -14,6 +14,7 @@ from scipy import signal
 import glob
 import wavSplit
 from timeit import default_timer as timer
+from morethings import VADAudio
 
 
 def main(ARGS):
@@ -30,27 +31,55 @@ def main(ARGS):
 	if ARGS.ref:
 		referencephrase=ARGS.ref
 		
-	ds = deepspeech.Model("ds-model.tflite")
-	ds.enableExternalScorer("ds-model.scorer")
+	model = deepspeech.Model("ds-model.tflite")
+	model.enableExternalScorer("ds-model.scorer")
 		
 	while cont:
 		cont=ARGS.cont
+		vad_audio = VADAudio(aggressiveness=1, device=None, input_rate=16000, file=None)
+		spinner = Halo(spinner='line')
+		print("Say something!")
+		spinner.start()
+		frames = vad_audio.vad_collector()
+		stream_context = model.createStream()
+		audio = bytearray()
+		t1=None
+		for frame in frames:
+			if frame is not None:
+				if t1 is None:
+					t1=time.time()
+				stream_context.feedAudioContent(np.frombuffer(frame, np.int16))
+				audio.extend(frame)
+			else:
+				#vad_audio.write_wav(datetime.now().strftime("savewav_%Y-%m-%d_%H-%M-%S_%f.wav"), audio)
+				spinner.stop()
+				resds = stream_context.finishStream()
+				tds= round((time.time()-t1)*100)/100
+				vad_audio.write_wav("rec.wav", audio)
+				print("Recognized: " + resds + " ("+str(tds)+" s)")
+				break
 		# obtain audio from the microphone
 		r = sr.Recognizer()
-		with sr.Microphone() as source:
-			r.adjust_for_ambient_noise(source)
-			spinner = Halo(spinner='line')
-			spinner.start()
-			print("Say something!")
-			out=0
-			audio = r.listen(source)
-			spinner.stop()
-			audioW=audio.get_wav_data()
-			print(audioW)
-			#fs = audioW.sample_rate
+		audio=sr.AudioData(audio, 16000, 2)
+		# with sr.Microphone() as source:
+			# r.adjust_for_ambient_noise(source)
+			# spinner = Halo(spinner='line')
+			# spinner.start()
+			# print("Say something!")
+			# out=0
+			# audio = r.listen(source)
+			# spinner.stop()
+			# print(audio.__str__())
+			# audioW=audio.get_wav_data(convert_rate=16000)
+			# filetosave=open("rec.wav","wb")
+			# filetosave.write(audioW)
+			# filetosave.close()
+			# #print(vad_segment_generator("rec.wav",1))
+			# #print(audioW)
+			# #fs = audioW.sample_rate
 			
-		# deepspeech part
-		#print(stt(ds,audioW,16000))
+		# # deepspeech part
+		# print(stt(ds,"rec.wav",16000))
 		
 			
 		# recognize speech using Google Speech Recognition
@@ -126,101 +155,3 @@ if __name__ == '__main__':
 	
 	ARGS = parser.parse_args()
 	main(ARGS)
-	
-	
-####
-# Altro codicde da eempio
-###
-'''
-
-import glob
-import webrtcvad
-import logging
-import wavSplit
-from deepspeech import Model
-from timeit import default_timer as timer
-
-
-#Load the pre-trained model into the memory
-@param models: Output Grapgh Protocol Buffer file
-@param scorer: Scorer file
-
-@Retval
-#Returns a list [DeepSpeech Object, Model Load Time, Scorer Load Time]
-
-def load_model(models, scorer):
-    model_load_start = timer()
-    ds = Model(models)
-    model_load_end = timer() - model_load_start
-    logging.debug("Loaded model in %0.3fs." % (model_load_end))
-
-    scorer_load_start = timer()
-    ds.enableExternalScorer(scorer)
-    scorer_load_end = timer() - scorer_load_start
-    logging.debug('Loaded external scorer in %0.3fs.' % (scorer_load_end))
-
-    return [ds, model_load_end, scorer_load_end]
-
-
-#Run Inference on input audio file
-@param ds: Deepspeech object
-@param audio: Input audio for running inference on
-@param fs: Sample rate of the input audio file
-
-@Retval:
-Returns a list [Inference, Inference Time, Audio Length]
-
-
-def stt(ds, audio, fs):
-    inference_time = 0.0
-    audio_length = len(audio) * (1 / fs)
-
-    # Run Deepspeech
-    logging.debug('Running inference...')
-    inference_start = timer()
-    output = ds.stt(audio)
-    inference_end = timer() - inference_start
-    inference_time += inference_end
-    logging.debug('Inference took %0.3fs for %0.3fs audio file.' % (inference_end, audio_length))
-
-    return [output, inference_time]
-
-
-Resolve directory path for the models and fetch each of them.
-@param dirName: Path to the directory containing pre-trained models
-
-@Retval:
-Retunns a tuple containing each of the model files (pb, scorer)
-
-def resolve_models(dirName):
-    pb = glob.glob(dirName + "/*.pbmm")[0]
-    logging.debug("Found Model: %s" % pb)
-
-    scorer = glob.glob(dirName + "/*.scorer")[0]
-    logging.debug("Found scorer: %s" % scorer)
-
-    return pb, scorer
-
-
-Generate VAD segments. Filters out non-voiced audio frames.
-@param waveFile: Input wav file to run VAD on.0
-
-@Retval:
-Returns tuple of
-    segments: a bytearray of multiple smaller audio frames
-              (The longer audio split into mutiple smaller one's)
-    sample_rate: Sample rate of the input audio file
-    audio_length: Duraton of the input audio file
-
-
-def vad_segment_generator(wavFile, aggressiveness):
-    logging.debug("Caught the wav file @: %s" % (wavFile))
-    audio, sample_rate, audio_length = wavSplit.read_wave(wavFile)
-    assert sample_rate == 16000, "Only 16000Hz input WAV files are supported for now!"
-    vad = webrtcvad.Vad(int(aggressiveness))
-    frames = wavSplit.frame_generator(30, audio, sample_rate)
-    frames = list(frames)
-    segments = wavSplit.vad_collector(sample_rate, 30, 300, vad, frames)
-
-    return segments, sample_rate, audio_length
-'''
